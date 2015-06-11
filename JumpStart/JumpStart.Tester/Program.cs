64ba024a49a6448165e2c16623706aca2c841984
vlad <vsh1818@gmail.com>
2015-06-11 17:10:52 +0300

    added configuration for newtonsoft

--- JumpStart/JumpStart.Tester/Program.cs
+++ JumpStart/JumpStart.Tester/Program.cs
@@ -10,6 +10,8 @@ namespace JumpStart.Tester
     {
         static void Main(string[] args)
         {
+            var result = MasterCardAPI.APIConnector.PayFromCreditCard(MasterCardAPI.APIConnector.CreateCreditCard("123",10,16,"5555555555554444"),1000, MasterCardAPI.CurrencyType.USD);
+            Console.ReadKey();
         }
     }
 }
