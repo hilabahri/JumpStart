89271230c2323f2118fcc362ef23c5bd982ec133
vlad <vsh1818@gmail.com>
2015-06-12 00:58:48 +0300

    added logics

--- JumpStart/JumpStartUI/Controllers/HomeController.cs
+++ JumpStart/JumpStartUI/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using Core.Entities;
 using DAL;
+using JumpStart.APILogics;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using System;
@@ -19,53 +20,12 @@ namespace JumpStartUI.Controllers
 
         public double GetCollectedAmountForDonatedCourse(string donatedID, string courseID)
         {
-            double sum = 0;
-            try{
-                foreach( Transaction tran in DataManager.Instance.GetTransactionsByDonatedId(donatedID))
-               {
-                   if (tran.CourseID == courseID) {
-                       sum++;
-                   }
-               }
-            }
-            catch(Exception)
-            {
-            }           
-           return sum;
+            return Logics.GetCollectedAmountForDonatedCourse(donatedID, courseID);
         }
 
         public JObject GetFundingRequestsMetaData()
         {
-            List<Donated> donated_list = DataManager.Instance.GetAllDonatedUsers();
-            JObject metaData = new JObject();
-            JArray metaDataArr = new JArray();
-            try
-            {
-                foreach (Donated donatedU in donated_list)
-            {
-                foreach (FundRequest fr in donatedU.FundRequests)
-                {
-                    JObject fundRequestMetaData = new JObject();
-                    fundRequestMetaData.Add("donaterID", donatedU.Id);
-                    fundRequestMetaData.Add("courseID", fr.CourseID);
-                    fundRequestMetaData.Add("course", DataManager.Instance.GetCourseDetails(fr.CourseID).CourseName);
-                    fundRequestMetaData.Add("age", (DateTime.Now.Year - donatedU.DateOfBirth.Year).ToString());
-                    fundRequestMetaData.Add("city",donatedU.UserAddress.City);
-                    fundRequestMetaData.Add("collectedAmount", GetCollectedAmountForDonatedCourse(donatedU.Id, fr.CourseID).ToString() + "$");
-                    fundRequestMetaData.Add("goalAmount", DataManager.Instance.GetCourseDetails(fr.CourseID).CoursePrice.ToString() + "$" );
-                    metaDataArr.Add(fundRequestMetaData);
-                }
-                
-            }
-            metaData.Add("aaData", metaDataArr);
-            //JObject metaData = JObject.Parse("{aaData : [{ \"purpose\" : \"liad\", \"age\" : \"something\", \"living_place\" : \"some office\", \"collected\" : \"103$\", \"goal\" : \"105$\" }]}");
-            }
-            catch(Exception)
-            {
-                return null;
-            }
-            
-            return metaData;
+            return Logics.GetFundingRequestsMetaData();
         }
 
         private List<CourseInstance> GetFundRequestCourseOptionalDates(string donatedID, string courseID)
