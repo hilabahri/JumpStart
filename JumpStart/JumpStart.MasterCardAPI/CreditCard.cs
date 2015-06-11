ef916e401738fdfde97efbb033fb0279b3d17c2f
vlad <vsh1818@gmail.com>
2015-06-11 14:34:39 +0300

    credit card

--- JumpStart/JumpStart.MasterCardAPI/CreditCard.cs
+++ JumpStart/JumpStart.MasterCardAPI/CreditCard.cs
@@ -2,11 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace JumpStart.MasterCardAPI
 {
-    public static class APIConnector
+    class CreditCard
     {
+        private string _id { get; }
+
     }
 }
